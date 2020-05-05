b53c4000a0392cc0da97f5e2571479e34dbe1f2c
Heidi <info@risako.fi>
2020-05-05 12:45:21 +0300

    Done

--- part1/conditionals/exercise_31/Program.cs
+++ part1/conditionals/exercise_31/Program.cs
@@ -2,12 +2,24 @@
 
 namespace exercise_31
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // Write your code here:
+        public static void Main(string[] args)
+        {
+            // Write your code here:
+            Console.WriteLine("Give a number:");
+            string number = Console.ReadLine();
+            int integer = Convert.ToInt32(number);
 
+            if ((integer % 2) == 0)
+            {
+              Console.WriteLine("It is even.");
+            }
+            else
+            {
+              Console.WriteLine("It is odd.");
+            }
+
+        }
     }
-  }
 }
