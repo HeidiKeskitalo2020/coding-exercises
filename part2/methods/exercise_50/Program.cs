41216f9134d64ba8a53d1f8b6751e097708f23f1
Heidi <info@risako.fi>
2020-06-28 12:43:06 +0300

    Done

--- part2/methods/exercise_50/Program.cs
+++ part2/methods/exercise_50/Program.cs
@@ -7,12 +7,18 @@ namespace exercise_50
     public static void Main(string[] args)
     {
       // Call your method here:
- 
+      Console.WriteLine("How many times?");
+      int number = Convert.ToInt32(Console.ReadLine());
+      for (int i = 0; i < number; i++) 
+      {
+        PrintPhrase();
+      }
     }
 
     // Write your method here:
-    public static void PrintPhrase() {
-
+    public static void PrintPhrase() 
+    {
+      Console.WriteLine("In a hole in the ground there lived a method");
     }
   }
 }
