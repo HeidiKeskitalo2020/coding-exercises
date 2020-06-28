5e14c8b6adb1d2af332c0e2e7e9e2641f70f562e
Heidi <info@risako.fi>
2020-06-28 13:37:31 +0300

    Done

--- part2/methods/exercise_53/Program.cs
+++ part2/methods/exercise_53/Program.cs
@@ -7,11 +7,17 @@ namespace exercise_53
     public static void Main(String[] args)
     {
       // Call your method here:
-
+      Division(5, 2);
 
     }
 
     // Write your method here:
-
+    public static void Division(int numerator, int denominator)
+    {
+      //return numerator / denominator;
+      
+      Console.WriteLine((double)numerator / (double)denominator);
+    }
+    
   }
 }
