785cf2c3b8d20a67a08357f40fe750d30808d69a
Heidi <info@risako.fi>
2020-06-28 14:17:55 +0300

    Done

--- part2/methods/exercise_59/Program.cs
+++ part2/methods/exercise_59/Program.cs
@@ -11,6 +11,22 @@ namespace exercise_59
     }
 
     // Write your method here:
-
+    public static int Greatest(int number1, int number2, int number3) 
+    {
+      int answer = 0;
+      if (number1 > number2 && number2 > number3)
+      {
+        answer = number1;
+      }
+      else if (number1 < number2 && number2 > number3)
+      {
+        answer = number2;
+      }
+      else
+      {
+        answer = number3;
+      }
+      return answer;
+    }
   }
 }
