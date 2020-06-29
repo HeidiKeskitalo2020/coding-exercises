9b23447c444a8073b94654a9bb8bb1e54482dbf3
Heidi <info@risako.fi>
2020-06-29 14:48:32 +0300

    Done

--- part3/lists/exercise_74/Program.cs
+++ part3/lists/exercise_74/Program.cs
@@ -17,8 +17,23 @@ namespace exercise_74
         }
         list.Add(input);
       }
+      Console.WriteLine("Search for?");
+      string input2 = Console.ReadLine();
+
       
-    }
+      //for ( int i = 0; i < list.Count; i++)
+      {
+        //string words  = list[i];
+        if (list.Contains (input2))
+        {
+          Console.WriteLine(input2 + " was found!");
+        }
+        else 
+        {
+          Console.WriteLine(input2 + " was not found!");
+        }
+      }
+    }  
   }
 }
 
