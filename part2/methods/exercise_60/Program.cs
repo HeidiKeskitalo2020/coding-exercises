cb2a7606e868d00f2cd5f2407ab9ecf556825732
Heidi <info@risako.fi>
2020-06-29 09:59:42 +0300

    Done

--- part2/methods/exercise_60/Program.cs
+++ part2/methods/exercise_60/Program.cs
@@ -4,9 +4,14 @@ namespace exercise_60
 {
   class Program
   {
-        public static void Main(String[] args)
+    public static void Main(String[] args)
     {
-
+      /*PrintStars(5);
+      PrintStars(3);
+      PrintStars(9);*/
+      //PrintSquare(4);
+      //PrintRectangle(17, 3);
+      PrintTriangle(4);
     }
 
 
@@ -17,21 +22,44 @@ namespace exercise_60
       // call the print command n times
       // in the end print a line break with the comand
       // Console.WriteLine("");
+      int i = 0;
+      while (i < number) 
+      {
+        Console.Write("*");
+        i++;
+      }
+      Console.WriteLine("");
     }
 
     public static void PrintSquare(int size)
     {
-
+      int number = 1; //for (int i = 0; i < size; i++)
+      while (number <= size)
+      {
+        PrintStars(size);
+        number++;
+      }
     }
 
     public static void PrintRectangle(int width, int height)
     {
-
+      for (int i = 0; i < height; i++)
+      {
+        //for (int j = 0; j < height; j++)
+        PrintStars(width);
+        //width++;
+      }
     }
 
     public static void PrintTriangle(int size)
     {
-
-    }
+      //for (int i = 0; i < size; i++)
+      int i = 0;
+      while (i < size)
+      {
+        PrintStars(i + 1);
+        i++;
+      }
+    }    
   }
 }
--- part2/methods/exercise_61/Program.cs
+++ part2/methods/exercise_61/Program.cs
@@ -6,22 +6,40 @@ namespace exercise_61
   {
     public static void Main(String[] args)
     {
-
+      PrintRightTriangle(4);
     }
 
     public static void PrintStars(int number)
     {
-
+      int i = 0;
+      while (i < number) 
+      {
+        Console.Write("*");
+        i++;
+      }
+      Console.WriteLine("");
     }
 
     public static void PrintSpaces(int number)
     {
-
+      int i = 1;
+      while (i < number) 
+      {
+        Console.Write(" ");
+        i++;
+      }
     }
 
     public static void PrintRightTriangle(int size)
     {
-
+      int i = 0;
+      while (i < size)
+      {
+        
+        PrintStars(i + 1);
+        i++;
+        PrintSpaces(i);
+      }
     }
 
     public static void ChristmasTree(int height)
