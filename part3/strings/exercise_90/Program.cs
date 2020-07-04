6b040c316f1a513de4261940e24f71e148a814d5
Heidi <info@risako.fi>
2020-07-04 15:06:10 +0300

    Done

--- part3/strings/exercise_90/Program.cs
+++ part3/strings/exercise_90/Program.cs
@@ -7,8 +7,25 @@ namespace exercise_90
   {
     public static void Main(string[] args)
     {
-
-
+      int oldest = 0;
+      while (true)
+      {
+        string nameAge = Console.ReadLine();
+        if ( nameAge == "")
+        {
+          break;
+        }
+        string[] pieces = nameAge.Split(",");
+      
+        
+        //int oldest = nameAge[1];
+        //for(int i = 0; i < oldest; i++) 
+        if (oldest < Convert.ToInt32(pieces[1])) 
+        {
+          oldest = Convert.ToInt32(pieces[1]);
+        }
+      }      
+      Console.WriteLine("Age of the oldest: " + oldest);
     }
   }
 }
