42c5ebfa8fdda9a80424d5878a26d8b95225c19e
Heidi <info@risako.fi>
2020-07-04 14:25:02 +0300

    Done

--- part3/strings/exercise_88/Program.cs
+++ part3/strings/exercise_88/Program.cs
@@ -7,7 +7,20 @@ namespace exercise_88
   {
     public static void Main(string[] args)
     {
-
+       while (true)
+      {
+        string input = Console.ReadLine();
+        if (input == "")
+        {
+          break;
+        }
+        string[] pieces = input.Split(" ");
+        //int i = 0;
+        //while (i < pieces.Length)
+        {
+          Console.WriteLine(pieces[0]);
+        }  
+      }  
     }
   }
 }
