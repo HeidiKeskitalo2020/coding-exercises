09faa11401f5e95b950dc0225606ed43fb1ad2be
Heidi <info@risako.fi>
2020-07-04 14:36:57 +0300

    Done

--- part3/strings/exercise_89/Program.cs
+++ part3/strings/exercise_89/Program.cs
@@ -7,7 +7,20 @@ namespace exercise_89
   {
     public static void Main(string[] args)
     {
-
+       while (true)
+      {
+        string input = Console.ReadLine();
+        if (input == "")
+        {
+          break;
+        }
+        string[] pieces = input.Split(" ");
+        //for (int i = 0; i < pieces.Length; i++)
+        {
+          int lastWord = pieces.Length;
+          Console.WriteLine(pieces[lastWord -1]);
+        }  
+      }  
 
     }
   }
