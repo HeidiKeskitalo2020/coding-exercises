7222ede81c784d3fbf7989a30829f08227d20404
Heidi <info@risako.fi>
2020-07-06 14:07:40 +0300

    Done

--- part4/objects/exercise_94/Program.cs
+++ part4/objects/exercise_94/Program.cs
@@ -8,7 +8,17 @@ namespace exercise_94
     {
       // DO NOT TOUCH THE OTHER FILE!
       // Do your code here!
+      Account heikkisAccount = new Account("Heikki's account", 1000.00);
+      Account personalAccount = new Account("Personal account", 0);
 
+      heikkisAccount.Withdrawal(100);
+      //Console.WriteLine("The balance of Heikki's account is now: " + heikkisAccount.balance);
+      personalAccount.Deposit(100);
+      //Console.WriteLine("The balance of Heikki's other account is now: " + heikkisSwissAccount.balance);
+
+      //Console.WriteLine("End state");
+      Console.WriteLine(heikkisAccount);
+      Console.WriteLine(personalAccount);
     }
   }
 }
