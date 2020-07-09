75f3440ca18931caf37578b03415612fb4a9679b
Heidi <info@risako.fi>
2020-07-09 18:36:26 +0300

    Done

--- part4/objects/exercise_105/Statistics.cs
+++ part4/objects/exercise_105/Statistics.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace exercise_105
 {
   public class Statistics
@@ -7,12 +9,25 @@ namespace exercise_105
 
     public Statistics()
     {
+      this.sum = 0;
+      this.count = 0;
+      //while(true)
+      //int count = 0;
       // initialize the variables here
+      //if (count == 0)
+      //{
+        //break;  
+      //}
+      //count = count++;      //count++;
     }
 
     public void AddNumber(int number)
     {
+      //this.number = number;
       // write code here
+      //this.number = number; 
+      this.count++;
+      this.sum = sum + number;
       
     }
   }
