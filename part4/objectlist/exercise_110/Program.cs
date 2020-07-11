32e039c2ab9d60751eeb7e2fab244969ccab1ea0
Heidi <info@risako.fi>
2020-07-11 14:26:20 +0300

    Done

--- part4/objectlist/exercise_110/Program.cs
+++ part4/objectlist/exercise_110/Program.cs
@@ -7,10 +7,28 @@ namespace exercise_110
   {
     public static void Main(string[] args)
     {
+      List<Item> items = new List<Item>();
       // IMPLEMENT YOUR CODE IN HERE!
+      
+      while (true)
+      {
+        Console.Write("Name: ");
+        string name = Console.ReadLine();
+        if (name == "")
+        {
+          break;
+        }
+        items.Add(new Item(name));
+      }
+      Console.WriteLine("");
+      foreach (Item item in items)  
+      {
+        Console.WriteLine(item);
+      }
     }
   }
 }
+
 
 
 
