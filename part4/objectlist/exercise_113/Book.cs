4d9f9c2556ff24758e3deb68a6919d4e1e5c7ddf
Heidi <info@risako.fi>
2020-07-11 15:50:10 +0300

    Done

---
+++ part4/objectlist/exercise_113/Book.cs
@@ -0,0 +1,24 @@
+using System;
+namespace exercise_113
+{
+    public class Book
+    {
+        public string name {get; set;}
+        public string pages{get; set;}
+        public string year {get; set;}
+
+        public Book(string name, string pages, string year)
+        {
+            this.name = name;
+            this.pages = pages;
+            this.year = year;
+        }
+        public override string ToString()
+        {
+            return this.name + ", " + this.pages + " pages, " + this.year;
+        }
+
+    }
+       
+}
+
--- part4/objectlist/exercise_113/Program.cs
+++ part4/objectlist/exercise_113/Program.cs
@@ -7,13 +7,40 @@ namespace exercise_113
   {
     public static void Main(string[] args)
     {
+      List<Book> list = new List<Book>();
+      while (true)
+      {
       // IMPLEMENT YOUR CODE IN HERE!
       // Remember also to create the file Book.cs and class in it!
+        Console.Write("Name: ");
+        string name = Console.ReadLine();
+        if (name == "")
+        {
+          break;
+        }
+        Console.Write("Pages: ");
+        string pages = Console.ReadLine();
+        Console.Write("Publication year: ");
+        string year = Console.ReadLine();
+
+        list.Add(new Book(name, pages, year));
+        }  
+        Console.WriteLine("");
+        
+        Console.Write("What information will be printed? ");
+        string what = Console.ReadLine();
+        foreach (Book item in list)
+        if ( what == "everything")
+        {
+          Console.WriteLine(item);
+        }
+        else if ( what == "title")
+        {
+          Console.WriteLine(item.name);
+        }
 
     }
   }
 }
-
-
 
 
