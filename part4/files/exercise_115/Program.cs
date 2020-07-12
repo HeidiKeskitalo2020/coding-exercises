5069e7bebea72265c0c74385b1aadaae15d24c2e
Heidi <info@risako.fi>
2020-07-12 14:09:33 +0300

    Done

--- part4/files/exercise_115/Program.cs
+++ part4/files/exercise_115/Program.cs
@@ -6,7 +6,20 @@ namespace exercise_115
   {
     public static void Main(string[] args)
     {
+      //int sum = 0;
+      while (true)
+      {
+        string number = Console.ReadLine();
 
+        if (number == "end") 
+        {
+          break;
+        }
+        int numbers = Convert.ToInt32(number);
+        //sum++;
+        Console.WriteLine(numbers * numbers * numbers);        
+      }
+      
     }
   }
 }
