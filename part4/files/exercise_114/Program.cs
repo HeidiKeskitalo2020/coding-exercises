c0b83c21bec2f3ca2757239bfb01ebfede248b4e
Heidi <info@risako.fi>
2020-07-12 13:57:13 +0300

    Done

--- part4/files/exercise_114/Program.cs
+++ part4/files/exercise_114/Program.cs
@@ -6,7 +6,22 @@ namespace exercise_114
     {
         public static void Main(string[] args)
         {
+            //List<string> string = new List<string>();
+           int count = 0;
+            while (true)
+            {
+                string line = Console.ReadLine();
 
+                if (line == "end") 
+                {
+                    break;
+                }
+                count++;
+                //int number = Convert.ToInt32(line);
+                //string[] strings = new string[number];
+               
+            }
+            Console.WriteLine(count);
         }
     }
 }
