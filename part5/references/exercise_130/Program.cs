062c243a34c673690b760d1b69a5ddd589023ab4
Heidi <info@risako.fi>
2020-07-22 11:02:35 +0300

    Done

--- part5/references/exercise_130/Program.cs
+++ part5/references/exercise_130/Program.cs
@@ -24,13 +24,20 @@ namespace exercise_130
 
         // BEGIN SOLUTION
         // Add unique books to the list.
+        //books.Add(book);
         // Remember to print
         // "The book is already on the list. Let's not add the same book again."
         // If the list Contains the book
+        if (books.Contains(book))
+        {
+          Console.WriteLine("The book is already on the list. Let's not add the same book again.");
+        }
+        else
+        {
+          books.Add(book);
 
-
-
-
+        }
+        
         // END SOLUTION
       }
 
