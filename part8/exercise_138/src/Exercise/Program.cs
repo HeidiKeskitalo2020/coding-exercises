702ef10c69ccd92c7b780ff072746ea4ccbde75c
Heidi <info@risako.fi>
2020-08-14 14:51:59 +0300

    Done

--- part8/exercise_138/src/Exercise/Program.cs
+++ part8/exercise_138/src/Exercise/Program.cs
@@ -8,7 +8,15 @@ namespace Exercise
     public static void Main(string[] args)
     {
       // Do something here
+      Dictionary<string, string> nickname = new Dictionary<string, string>();
+      nickname.Add("matthew", "matt");
+      nickname.Add("michael", "mix");
+      nickname.Add("arthur", "artie");
 
+      foreach (KeyValuePair<string, string> kvp in nickname)
+      {
+          Console.WriteLine("{0}'s nickname is {1}",
+          kvp.Key, kvp.Value);
       }
     }
   }
