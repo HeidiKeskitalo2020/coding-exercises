01ba62f2654962291c7d87008d76e10482fbf210
Heidi <info@risako.fi>
2020-08-18 10:21:33 +0300

    Done

--- part8/exercise_142/src/Exercise/Program.cs
+++ part8/exercise_142/src/Exercise/Program.cs
@@ -7,6 +7,13 @@ namespace Exercise
   {
     public static void Main(string[] args)
     {
+      /*IOU mattsIOU = new IOU();
+      mattsIOU.ChangeDebt("Arthur", 51);
+      mattsIOU.ChangeDebt("Michael", 30);
+
+      Console.WriteLine(mattsIOU.HowMuchDoIOweTo("Arthur"));
+      Console.WriteLine(mattsIOU.HowMuchDoIOweTo("Michael"));
+      Console.WriteLine(mattsIOU.HowMuchDoIOweTo("Heikki"));*/
       IOU mattsIOU = new IOU();
       mattsIOU.ChangeDebt("Arthur", 51);
       mattsIOU.ChangeDebt("Arthur", 30);
