733a83959e70c989fbf5d967a802250ab77d39a9
Heidi <info@risako.fi>
2020-08-20 10:47:06 +0300

    Done

--- part9/exercise_149/src/Exercise/People/Student.cs
+++ part9/exercise_149/src/Exercise/People/Student.cs
@@ -1,6 +1,23 @@
 namespace Exercise
 {
-  public class Student
+  public class Student: Person
   {
+    //private int credits;
+    public int credits {get; set;}
+    //private int credits;
+    public Student(string name, string address) :
+      base(name, address)
+    {
+      this.credits = credits;
+    }    
+    public void Study()
+    {
+      this.credits++;
+    }
+
+    public override string ToString()
+    {
+      return base.ToString() + " credits: " + this.credits;
+    }
   }
 }
