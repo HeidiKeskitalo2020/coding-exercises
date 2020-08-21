5878212b24e30f4390c4681f7a3dc41c21f66c3b
Heidi <info@risako.fi>
2020-08-21 16:02:12 +0300

    Done

--- part9/exercise_150/src/Exercise/Warehouses/ChangeHistory.cs
+++ part9/exercise_150/src/Exercise/Warehouses/ChangeHistory.cs
@@ -1,39 +1,54 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
 namespace Exercise
 {
-  using System;
-  using System.Collections.Generic;
   public class ChangeHistory
   {
     private List<int> history;
 
     public ChangeHistory()
     {
+      this.history = new List<int>();
 
     }
 
     public void Add(int status)
     {
-
+      this.history.Add(status);
     }
 
     public void Clear()
     {
-
+      this.history.Clear();
     }
 
     public int MaxValue()
     {
-      return 0;
+      int maxValue = 0;
+      if (this.history.Count > 0)
+      {
+        maxValue = this.history.Max();
+      }
+
+      return maxValue;
+      
     }
 
     public int MinValue()
     {
-      return 0;
+      int minValue = 0;
+      if (this.history.Count > 0)
+      {
+        minValue = this.history.Min();
+      }
+      return minValue;
     }
+    
 
     public override string ToString()
     {
-      return "";
+      return "Current: " + this.history.LastOrDefault() + " Min: " + MinValue() + " Max: " + MaxValue();
     }
   }
 }
--- part9/exercise_150/src/Exercise/Warehouses/ProductWareHouseWithHistory.cs
+++ part9/exercise_150/src/Exercise/Warehouses/ProductWareHouseWithHistory.cs
@@ -2,24 +2,43 @@ namespace Exercise
 {
   public class ProductWarehouseWithHistory : ProductWarehouse
   {
+    public int initialBalance;
+    public ChangeHistory changes;
     public ProductWarehouseWithHistory(string productName, int capacity, int initialBalance) : base(productName, capacity)
     {
-
+      this.initialBalance = initialBalance;
+      this.changes = new ChangeHistory();
+      AddToWarehouse(initialBalance);
     }
 
     public string History()
     {
-      return "";
+      return this.changes.ToString();
     }
 
     new public void AddToWarehouse(int amount)
     {
-
+      base.AddToWarehouse(amount);
+      this.changes.Add(this.balance);
+      /*if (amount <= 0)
+      {
+        return;
+      }
+      if (amount <= HowMuchSpaceLeft())
+      {
+        this.balance = this.balance + this.changes;
+      }
+      else
+      {
+        this.balance = this.capacity;
+      }*/
     }
 
     new public int TakeFromWarehouse(int amount)
     {
-      return 0;
+      base.TakeFromWarehouse(amount);
+      this.changes.Add(this.balance);
+      return this.balance;
     }
   }
 }
