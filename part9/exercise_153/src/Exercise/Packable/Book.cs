1749f7f345b5526fad6b4ed13d326e9b15fe1d56
Heidi <info@risako.fi>
2020-08-24 16:06:04 +0300

    Done

--- part9/exercise_153/src/Exercise/Packable/Book.cs
+++ part9/exercise_153/src/Exercise/Packable/Book.cs
@@ -2,19 +2,27 @@ namespace Exercise
 {
   public class Book : IPackable
   {
-
+    public string s;
+    public string s2;
+    public int i;
+    public int weight;
+    
     public Book(string s, string s2, int i)
     {
+      this.s = s;
+      this.s2 = s2;
+      this.i = i;
+      this.weight = 1;
     }
 
     public int Weight()
     {
-      return 0;
+      return this.weight;
     }
 
     public override string ToString()
     {
-      return "";
+      return this.s + ": " + this.s2 + " (" + this.i + ")";
     }
   }
 }
--- part9/exercise_153/src/Exercise/Packable/Furniture.cs
+++ part9/exercise_153/src/Exercise/Packable/Furniture.cs
@@ -2,19 +2,28 @@ namespace Exercise
 {
   public class Furniture : IPackable
   {
+    public string s;
+    public string s2;
+    public int i;
+    
+
     public Furniture(string s, string s2, int i)
     {
+      this.s = s;
+      this.s2 = s2;
+      this.i = i;
+      //this.weight = weight;
 
     }
 
     public int Weight()
     {
-      return 0;
+      return this.i;
     }
 
     public override string ToString()
     {
-      return "";
+      return this.s2 + " " + this.s + " - weight " + this.i + " kg";
     }
   }
 }
