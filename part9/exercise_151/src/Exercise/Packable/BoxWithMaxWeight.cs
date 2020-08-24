28ab2ab88ab382b25a5d0d9c5bca4a745a43fd02
Heidi <info@risako.fi>
2020-08-24 10:16:26 +0300

    Done

--- part9/exercise_151/src/Exercise/Packable/BoxWithMaxWeight.cs
+++ part9/exercise_151/src/Exercise/Packable/BoxWithMaxWeight.cs
@@ -1,20 +1,40 @@
 namespace Exercise
 {
+  using System.Linq;
   using System.Collections.Generic;
   public class BoxWithMaxWeight : Box
   {
+    public List<Item> list;
+    private int capacity;
     public BoxWithMaxWeight(int capacity)
     {
+      this.capacity = capacity;
+      this.list = new List<Item>();
     }
 
     public override void Add(Item item)
     {
+      if (this.capacity > item.weight)
+      {
+        int weightSumInList = list.Sum(item => item.weight);
+        if (weightSumInList < this.capacity)
+        {
+          list.Add(item);
+        }
+      }
     }
 
 
     public override bool IsInBox(Item item)
     {
-      return false;
+      if (list.Contains(item))
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
     }
   }
 }
--- part9/exercise_151/src/Exercise/Packable/MisplacingBox.cs
+++ part9/exercise_151/src/Exercise/Packable/MisplacingBox.cs
@@ -3,18 +3,31 @@ namespace Exercise
   using System.Collections.Generic;
   public class MisplacingBox : Box
   {
+    public List<Item> list;
     public MisplacingBox()
     {
+       this.list = new List <Item>();
     }
 
     public override void Add(Item item)
     {
+       //if (list.Count == 0)
+      {
+        list.Add(item);
+      }
     }
 
 
     public override bool IsInBox(Item item)
     {
-      return true;
+      /*if (list.Contains(item))
+      {
+        return false;
+      }
+      else*/
+      {
+        return false;
+      }
     }
   }
 }
