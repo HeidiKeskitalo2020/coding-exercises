4c62a6a0d8156a860f8d2a131dfa6b1c4f0447d1
Heidi <info@risako.fi>
2020-08-25 15:40:42 +0300

    Done

--- part10/exercise_157/src/Exercise/Student/Student.cs
+++ part10/exercise_157/src/Exercise/Student/Student.cs
@@ -20,8 +20,24 @@ namespace Exercise
     //BEGIN SOLUTION
     public int CompareTo(Student another)
     {
-
-      return 0;
+      return this.ToString().CompareTo(another.ToString());
+      /* Tämä malli äy vain numeroita verrattaessa:
+      if (another == null)
+      {
+        return 1;
+      }
+      if (this.name == another.name)
+      {
+        return 0;
+      }
+      else if (this.name > another.name)
+      {
+        return 1;
+      }
+      else
+      { 
+        return -1;
+      }*/  
     }
     //END SOLUTION
   }
