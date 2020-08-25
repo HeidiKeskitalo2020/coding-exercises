804deeedf6b57d1dd475f157ee15886a0f4d8d40
Heidi <info@risako.fi>
2020-08-25 13:55:28 +0300

    Done

--- part9/exercise_154/src/Exercise/Herd/Herd.cs
+++ part9/exercise_154/src/Exercise/Herd/Herd.cs
@@ -1,24 +1,40 @@
 namespace Exercise
 {
+  using System;
   using System.Collections.Generic;
   public class Herd : IMovable
   {
-
+    public int x;
+    public int y;
+    public List<IMovable> list;
     public Herd()
     {
+      //this.x = x;
+      //this.y = y;
+      this.list = new List<IMovable>();
     }
 
-    public void AddToHerd(IMovable m)
+    public void AddToHerd(IMovable movable)
     {
+      this.list.Add(movable);
     }
 
     public void Move(int dx, int dy)
     {
+      foreach (IMovable movable in list)
+      {
+        movable.Move(dx, dy);
+      }
     }
 
     public override string ToString()
     {
-      return "";
+      string listPosition = "";
+      foreach(IMovable movable in list)
+      {
+        listPosition += movable.ToString() + "\n";
+      }
+      return listPosition;
     }
   }
 }
--- part9/exercise_154/src/Exercise/Herd/Organism.cs
+++ part9/exercise_154/src/Exercise/Herd/Organism.cs
@@ -2,18 +2,25 @@ namespace Exercise
 {
   public class Organism : IMovable
   {
+    public int x {get; set;}
+    public int y {get; set;}
+    //private int dx;
+    //private int dy;
 
     public Organism(int x, int y)
     {
+      this.x = x;
+      this.y = y;
     }
     public void Move(int dx, int dy)
     {
-
+      this.x += dx;
+      this.y += dy;
     }
 
     public override string ToString()
     {
-      return "";
+      return "x: " + this.x + "; y: " + this.y;
     }
   }
 }
