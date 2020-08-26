561b172978ce3ade8b2eb139c461cd65638e4bc6
Heidi <info@risako.fi>
2020-08-26 11:09:13 +0300

    Done

--- part10/exercise_160/src/Exercise/CardGame/Hand.cs
+++ part10/exercise_160/src/Exercise/CardGame/Hand.cs
@@ -6,22 +6,57 @@ namespace Exercise
   {
     private List<Card> hand;
 
+     public Hand()
+    {
+      this.hand = new List<Card>();
+    }
+
     public void Add(Card card)
     {
+      if (!this.hand.Contains(card))
+      {
+        this.hand.Add(card);      
+      }
     }
 
     public void Print()
     {
+      hand.ForEach(Console.WriteLine);
     }
 
     public void Sort()
     {
+      this.hand.Sort();
     }
 
 
     public int CompareTo(Hand hand)
     {
-      return 0;
+      int handSum1 = 0;
+      foreach (Card card in this.hand)
+      {
+        handSum1 += card.value;
+      }
+
+      int handSum2 = 0;
+      foreach (Card card in hand.hand)
+      {
+        handSum2 += card.value;
+      }
+
+      
+      if (handSum1 == handSum2)
+      {
+        return 0;
+      }
+      else if (handSum1 > handSum2)
+      {
+        return 1;
+      }
+      else
+      {
+        return -1;
+      }
     }
   }
 }
